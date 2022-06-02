3fbd0409b7719e547d45969053bdbcb449961c43
Kyle R. I. Pla <64238454+kpla001@users.noreply.github.com>
2022-06-02 00:52:35 -0400

    fix ts complaints

--- components/HeadTag/helpers/HeadTag.helpers.tsx
+++ components/HeadTag/helpers/HeadTag.helpers.tsx
@@ -1,30 +1,25 @@
 import Script from 'next/script'
+import { HTMLAttributeReferrerPolicy } from 'react'
 import { HeadTagProps } from '../../../interfaces/HeadTag.interface'
 
 
 export const listLinks = (linkTags: HeadTagProps["links"]) => linkTags.map((link, index) => (
-    <>
         <link 
-            key={index} 
+            key={`${index}link`} 
             rel={link.rel} 
             href={link.href}
             integrity={link?.integrity ?? ''}
             crossOrigin={link?.crossOrigin ?? ''}
-            referrerPolicy={link?.referrerPolicy ?? ''}
+            referrerPolicy={link?.referrerPolicy as HTMLAttributeReferrerPolicy ?? ''}
         />
-        <br/>
-    </> 
     )
 )
 export const listScripts = (scriptTags: HeadTagProps["scripts"]) => scriptTags.map((script, index) => ( 
-    <> 
         <Script 
-            key={index}
+            key={`${index}script`}
             id={`script-${index}`}
             dangerouslySetInnerHTML={script?.dangerouslySetInnerHTML}
             async src={script?.src}
         />
-        <br/>
-    </>
     )
 )
